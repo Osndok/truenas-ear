75da12f6fb36152bfe40824b97d7c6a2434d6bbf
Robert Hailey <git@osndok.com>
2023-11-27 16:12:08 -0600

    Reorganize code/braces

--- src/main.zig
+++ src/main.zig
@@ -12,17 +12,20 @@ const CString = [*:0]const u8;
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 var allocator = gpa.allocator();
 
-const Subcommand = enum {
+const Subcommand = enum
+{
     lock,
     unlock,
     @"is-locked",
     @"is-unlocked"
 };
 
-pub fn main() !void {
+pub fn main() !void
+{
     const argv : []CString = std.os.argv;
 
-    if (argv.len != 3) {
+    if (argv.len != 3)
+    {
         try usage();
         std.os.exit(1);
     }
@@ -33,12 +36,16 @@ pub fn main() !void {
     _ = it.next();
 
     const command = subcommand: {
-        const subc_string = it.next() orelse {
+        const subc_string = it.next()
+        orelse
+        {
             try usage();
             std.os.exit(1);
         };
 
-        break :subcommand std.meta.stringToEnum(Subcommand, subc_string) orelse {
+        break :subcommand std.meta.stringToEnum(Subcommand, subc_string)
+        orelse
+        {
             try err.print("invalid command: {s}\n", .{subc_string});
             try usage();
             std.os.exit(1);
@@ -47,7 +54,8 @@ pub fn main() !void {
    
     const dataset = cstringToString(it.next().?);
 
-    try switch (command) {
+    try switch (command)
+    {
         .lock => lock(dataset),
         .unlock => unlock(dataset),
         .@"is-locked" => is_locked(dataset),
@@ -55,37 +63,45 @@ pub fn main() !void {
     };
 }
 
-fn usage() anyerror!void {
+fn usage() anyerror!void
+{
     try err.print("usage: {s} command dataset\n", .{std.os.argv[0]});
     try err.print("where: command in (lock, unlock, is-locked, is-unlocked)\n", .{});
 }
 
-fn cstringToString(input: CString) String {
+fn cstringToString(input: CString) String
+{
     var length = std.mem.len(input);
     return input[0..length];
 }
 
-fn run(argv: []const String) !String {
+fn run(argv: []const String) !String
+{
     var process = try child.exec(.{
         .allocator = allocator,
         .argv = argv
     });
 
-    if (process.term.Exited != 0) {
+    if (process.term.Exited != 0)
+    {
         log.err("process returned exit status {}: {any}\n{s}", .{process.term.Exited, argv, process.stderr});
         return std.os.ExecveError.Unexpected;
     }
 
+    // ???: need to dealloc captured stdout & stderr?
+
     return process.stdout;
 }
 
-const LockStatus = enum {
+const LockStatus = enum
+{
     locked,
     unlocked,
     unknown
 };
 
-fn getLockStatus(dataset: String) !LockStatus {
+fn getLockStatus(dataset: String) !LockStatus
+{
     var line = try run(&[_]String {
         "zfs", "get", "-H", "keystatus", dataset
     });
@@ -97,40 +113,47 @@ fn getLockStatus(dataset: String) !LockStatus {
     const statusEtc = afterTab[tab2+1..];
     log.debug("statusEtc = {s}", .{statusEtc});
 
-    if (std.mem.startsWith(u8, statusEtc, "unavailable")) {
+    if (std.mem.startsWith(u8, statusEtc, "unavailable"))
+    {
         return LockStatus.locked;
     }
 
-    if (std.mem.startsWith(u8, statusEtc, "available")) {
+    if (std.mem.startsWith(u8, statusEtc, "available"))
+    {
         return LockStatus.unlocked;
     }
 
     return LockStatus.unknown;
 }
 
-fn is_locked(dataset: String) !void {
+fn is_locked(dataset: String) !void
+{
     var status = try getLockStatus(dataset);
     log.info("{s} is {!}", .{dataset, status});
 
-    switch (status) {
+    switch (status)
+    {
         .locked => std.os.exit(0),
         .unknown => std.os.exit(1),
         .unlocked => std.os.exit(2)
     }
 }
 
-fn is_unlocked(dataset: String) !void {
+fn is_unlocked(dataset: String) !void
+{
     var status = try getLockStatus(dataset);
     log.info("{s} is {!}", .{dataset, status});
 
-    switch (status) {
+    switch (status)
+    {
         .unlocked => std.os.exit(0),
         .unknown => std.os.exit(1),
         .locked => std.os.exit(2),
     }
 }
 
-fn concat(one: String, two: String) !String {
+fn concat(one: String, two: String) !String
+{
     //var b = try std.Buffer.init(allocator, one);
     //try b.append(two);
     //return b;
@@ -140,11 +163,8 @@ fn concat(one: String, two: String) !String {
     return result;
 }
 
-fn unlock(dataset: String) !void {
-    log.info("unlock: {s}", .{dataset});
-
-    // (1) zfs load-key $DATASET
-
+fn zfs_load_key(dataset: String) !void
+{
     var process = child.init(&[_]String{
         "zfs", "load-key", dataset
     }, allocator);
@@ -157,15 +177,17 @@ fn unlock(dataset: String) !void {
     try process.spawn();
     var status = try process.wait();
     
-    if (status.Exited != 0) {
+    if (status.Exited != 0)
+    {
         log.err("exit-status {}: zfs load-key {s}", .{status.Exited, dataset});
         return std.os.ExecveError.Unexpected;
     }
     //???: process.deinit();
+}
 
-    // (2) zfs mount $DATASET
-
-    process = child.init(&[_]String{
+fn zfs_mount(dataset: String) !void
+{
+    var process = child.init(&[_]String{
         "zfs", "mount", dataset
     }, allocator);
     {
@@ -175,12 +197,17 @@ fn unlock(dataset: String) !void {
     }
     
     try process.spawn();
-    status = try process.wait();
+    var status = try process.wait();
     
-    if (status.Exited != 0) {
+    if (status.Exited != 0)
+    {
         log.warn("exit-status {}: zfs mount {s}", .{status.Exited, dataset});
     }
     //???: process.deinit();
+}
+
+fn do_post_unlock_followups(dataset: String) !void
+{
 
     // NB: Assuming '/mnt/' prefix (TrueNAS)
     const mountPoint = try concat("/mnt/", dataset);
@@ -194,7 +221,9 @@ fn unlock(dataset: String) !void {
     log.debug("followUpFile: {s}", .{followUpFile});
 
     // look for list of services to start, and scripts to execute: "post-mount:/some/script.sh"
-    var file = std.fs.cwd().openFile(followUpFile, .{}) catch {
+    var file = std.fs.cwd().openFile(followUpFile, .{})
+    catch
+    {
         log.info("dne: {s}", .{followUpFile});
         return;
     };
@@ -203,8 +232,10 @@ fn unlock(dataset: String) !void {
     var buf_reader = std.io.bufferedReader(file.reader());
     var in_stream = buf_reader.reader();
     var buf: [2048]u8 = undefined;
-    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        if (line.len == 0 or line[0] == '#' ) {
+    while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line|
+    {
+        if (line.len == 0 or line[0] == '#' )
+        {
             continue;
         }
 
@@ -213,7 +244,17 @@ fn unlock(dataset: String) !void {
     }
 }
 
-fn lock(dataset: String) !void {
+fn unlock(dataset: String) !void
+{
+    log.info("unlock: {s}", .{dataset});
+
+    try zfs_load_key(dataset);
+    try zfs_mount(dataset);
+    try do_post_unlock_followups(dataset);
+}
+
+fn lock(dataset: String) !void
+{
     log.info("lock: {s}", .{dataset});
     // stop services
     // wait?
